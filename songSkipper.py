638d8051a1a66e144dd2f34e6d66e49155e3667a
Diba Mirza <dimirza@eng.ucsd.edu>
2016-08-31 14:01:08 -0700

    finished code to skip songs

--- songSkipper.py
+++ songSkipper.py
@@ -1,5 +1,9 @@
 import RPi.GPIO as GPIO
 import time
+import playmusic
+import os
+from time import sleep
+import subprocess
 
 #set up the pins
 
@@ -13,47 +17,11 @@ def setup():
         ''' One time set up configurations'''
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(sensor, GPIO.IN)
-        '''for pin in pins.values():
-                GPIO.setup(pin, GPIO.OUT)
-                GPIO.output(pin, GPIO.LOW)
-        GPIO.setup(BtnPin, GPIO.IN, pull_up_down = GPIO.PUD_UP)'''
-        
 
-'''def reset():
-        for pin in pins.values():
-                GPIO.output(pin, GPIO.LOW)'''      
-
-'''def loop():
-        state = 0
-        while True:
-                # This code repeats forever
-                if state == 0:
-                    time.sleep(1)
-                        
-                if state == 1:
-                    time.sleep(1)
-
-                if state == 2:
-                    time.sleep(1)
-
-                if state == 3:
-                    reset()    
-                    time.sleep(1)
-
-                if state == 4:
-                    time.sleep(1)
-
-                if state == 5:
-                    time.sleep(1)
-
-                if state == 6:
-                    time.sleep(1)
-                    state = 0
-                state += 1'''
-
-def touchSensor():
+def touchSensor(file_list):
         state= 0
         songnum= 0
+        playsong(file_list[0])
         while True:
                 #print GPIO.input(sensor)
                 if state== 0 and GPIO.input(sensor)== False:
@@ -62,21 +30,47 @@ def touchSensor():
                         state=0
                         songnum = (songnum + 1) % tot_songs
                         print 'skipping to song ', songnum
+                        quitmusic()
+                        playsong(file_list[songnum])
+        quitmusic() 
                 
 
+def list_files(mypath):
+    file_list =[]
+    for f in os.listdir(mypath):
+        if os.path.isfile(os.path.join(mypath, f)):
+            file_list.append(f)
 
-                
+    return file_list
+
+def quitmusic():
+        sleep(1)
+        print "Pause track"
+        subprocess.call(['killall', 'mpg123'])
+
+def playsong(file_name):
+    print file_name
+    subprocess.Popen(['mpg123',file_name])
+    print "Playing .....", file_name
+    print "Duration "
+
+        
                 
 def destroy():
-        GPIO.cleanup()                     
+        GPIO.cleanup()
 
 if __name__ == '__main__':     # Program start from here
         setup()
+        path = "/media/USB DISK/"
+        os.chdir(path)
+        file_list = list_files('./')
+        print file_list
         try:
-                touchSensor()
+                touchSensor(file_list)
         except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
                 
                 destroy()
+                subprocess.call(['killall', 'mpg123'])
 
 
 
